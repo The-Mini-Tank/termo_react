46da7e9a60a36690b77ac71880aee9ea1c5956b4
EDUARDO SOUSA LIMA <eduardo.lima@mpap.local>
2024-11-07 11:18:33 -0300

    add rederização dinamica

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,245 +1,249 @@
-import { View, Text, Button, TextInput, TouchableOpacity, Alert, ToastAndroid  } from 'react-native';
+import { View, Text, Button, TextInput, TouchableOpacity, Alert, ToastAndroid } from 'react-native';
 import FlashMessage, { positionStyle } from 'react-native-flash-message';
 import { showMessage } from 'react-native-flash-message';
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import dict from '../dict.json';
 
 
 export default function HomeScreen({ navigation }) {
 
-    const [rowSet, setRowSet] = useState([0, 0, 0, 0, 0]);
-    const [inputText, setInputText] = useState('');
-    const [inputText2, setInputText2] = useState('');
-    const [inputText3, setInputText3] = useState('');
-    const [inputText4, setInputText4] = useState('');
-    const [inputText5, setInputText5] = useState('');
+  let linha = 0;
+  const initialViewCount = 5; // Número inicial de Views
+  const [rowSet, setRowSet] = useState([0, 0, 0, 0, 0]);
+  const [inputText, setInputText] = useState(['', '', '', '', '']);
+  const [showInput, setShowInput] = useState(false);
+  const [views, setViews] = useState(Array.from({ length: initialViewCount }, (_, i) => i));
 
-    const [palavraSecreta, setPalavraSecreta] = useState('');
-    console.log(palavraSecreta);
 
-    // Palavra secreta
-    useEffect(() => {
-      const escolherPalavraAleatoria = () => {
-        const indiceAleatorio = Math.floor(Math.random() * dict.palavras.length);
-        return dict.palavras[indiceAleatorio];
-      };
-      
-      setPalavraSecreta(escolherPalavraAleatoria());
-    }, []);
+  const [palavraSecreta, setPalavraSecreta] = useState('');
+  console.log(palavraSecreta);
 
-    const atualizarRowSet = (letra, index, novoRowSet) => {
-        for (let i = 0; i < palavraSecreta.length; i++) {
-          if (palavraSecreta[i] === letra) {
-            if(index === i){ 422
-                novoRowSet[index] = 2;
-            } else { 
-                novoRowSet[index] = 1;
-            } 
-          }
+  // Palavra secreta
+  useEffect(() => {
+    const escolherPalavraAleatoria = () => {
+      const indiceAleatorio = Math.floor(Math.random() * dict.palavras.length);
+      return dict.palavras[indiceAleatorio];
+    };
+
+    setPalavraSecreta(escolherPalavraAleatoria());
+  }, []);
+
+  useEffect(() => {
+    console.log("Estado atualizado de rowSet:", rowSet);
+  }, [rowSet]);
+
+  const addView = () => {
+    setViews([...views, views.length]);
+  };
+
+  const handleInputChange = (index, value) => {
+    setInputText(inputText => {
+      const newInputText = [...inputText];
+      newInputText[index] = value;
+      return newInputText;
+    });
+  };
+
+  const atualizarRowSet = (letra, index, novoRowSet) => {
+    let letra_encontrada = false;
+    for (let i = 0; i < palavraSecreta.length; i++) {
+      if (palavraSecreta[i] === letra) {
+        if (index === i) {
+          novoRowSet[index] = 2;
+          letra_encontrada = true;
+        } else {
+          novoRowSet[index] = 1;
+          letra_encontrada = true;
         }
-      };
-
-    useEffect(() => {
-        console.log("Estado atualizado de rowSet:", rowSet);
-    }, [rowSet]);
-
-    const getRectangleStyle = (tipo) => {
-        switch (tipo) {
-          case 1:
-            return styles.rectanglePurple;
-          case 2:
-            return styles.rectangleGreen;
-          default:
-            return styles.rectangle;
-        }
-      };
-
-    function formatarTexto(texto) {
-        return texto
-          .toLowerCase() 
-          .normalize("NFD") 
-          .replace(/[\u0300-\u036f]/g, ""); 
+      } else {
+        if (!letra_encontrada)
+          novoRowSet[index] = 3;
+      }
     }
+  };
 
-    // Configurar Botão
-    const TryButton = ({ title, onPress }) => {
-        const [isPressed, setIsPressed] = useState(false);
-      
-        return (
-          <TouchableOpacity
-            style={[styles.button, isPressed && styles.buttonActive]}
-            onPress={onPress}
-            activeOpacity={0.8}
-            onPressIn={() => setIsPressed(true)}
-            onPressOut={() => setIsPressed(false)}
-          >
-            <Text style={[styles.buttonText, isPressed && styles.buttonTextActive]}>
-              {title}
-            </Text>
-          </TouchableOpacity>
-        );
-      };
+  const getRectangleStyle = (tipo) => {
+    switch (tipo) {
+      case 1:
+        return styles.rectanglePurple;
+      case 2:
+        return styles.rectangleGreen;
+      case 3:
+        return styles.rectangleGray;
+      default:
+        return styles.rectangle;
+    }
+  };
 
-      
- 
-      const checkWordSpell = () => {
-        const values = [inputText, inputText2, inputText3, inputText4, inputText5];
-        const concatenatedString = values.join('');
+  function formatarTexto(texto) {
+    return texto
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+  }
 
-        let textoFormatado = formatarTexto(concatenatedString);
-
-        if (textoFormatado.length === 5) {
-            checkWord(textoFormatado);
-        } else {
-            showMessage({
-                message: "Palavra Incompleta!",
-                description: "Muito burro",
-                type: "warning",
-              });
-        }
-      }
-
-      const checkWord = (word) => {
-        if(word == palavraSecreta){
-            console.log('acertou');
-        } else {
-            const novoRowSet = [...rowSet];
-
-            word.split('').forEach((caractere, index) => {
-                atualizarRowSet(caractere, index, novoRowSet);
-            });
-            console.log(novoRowSet);
-            setRowSet(novoRowSet); 
-
-            showMessage({
-                message: "ERROU!",
-                description: "Muito burro",
-                type: "info",
-              });
-        }
-      }
-    
-      const handleInputChange = (setter, index) => (text) => {
-        setter(text);
-      };
+  // Configurar Botão
+  const TryButton = ({ title, onPress }) => {
+    const [isPressed, setIsPressed] = useState(false);
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text> {palavraSecreta}</Text> 
+      <TouchableOpacity
+        style={[styles.button, isPressed && styles.buttonActive]}
+        onPress={onPress}
+        activeOpacity={0.8}
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
+      >
+        <Text style={[styles.buttonText, isPressed && styles.buttonTextActive]}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
 
-        <View style={{ flexDirection: 'row', marginTop: 5 }}>
-            <View style={[getRectangleStyle(rowSet[0]), { justifyContent: 'center', alignItems: 'center' }]}>
-                <TextInput
-                    style={[styles.input, { width: '70%', textAlign: 'center' }]}
-                    placeholder="-"
-                    value={inputText}
-                    onChangeText={handleInputChange(setInputText, 0)}
-                    maxLength={1}
-                />
-            </View>
-            <View style={[getRectangleStyle(rowSet[1]), { justifyContent: 'center', alignItems: 'center' }]}>
-                <TextInput
-                    style={[styles.input, { width: '70%', textAlign: 'center' }]}
-                    placeholder="-"
-                    value={inputText2}
-                    onChangeText={handleInputChange(setInputText2, 1)}
-                    maxLength={1}
-                />
-            </View>
-            <View style={[getRectangleStyle(rowSet[2]), { justifyContent: 'center', alignItems: 'center' }]}>
-                <TextInput
-                    style={[styles.input, { width: '70%', textAlign: 'center' }]}
-                    placeholder="-"
-                    value={inputText3}
-                    onChangeText={handleInputChange(setInputText3, 2)}
-                    maxLength={1}
-                />
-            </View>
-            <View style={[getRectangleStyle(rowSet[3]), { justifyContent: 'center', alignItems: 'center' }]}>
-                <TextInput
-                    style={[styles.input, { width: '70%', textAlign: 'center' }]}
-                    placeholder="-"
-                    value={inputText4}
-                    onChangeText={handleInputChange(setInputText4, 3)}
-                    maxLength={1}
-                />
-            </View>
-            <View style={[getRectangleStyle(rowSet[4]), { justifyContent: 'center', alignItems: 'center' }]}>
-                <TextInput
-                    style={[styles.input, { width: '70%', textAlign: 'center' }]}
-                    placeholder="-"
-                    value={inputText5}
-                    onChangeText={handleInputChange(setInputText5, 4)}
-                    maxLength={1}
-                />
-            </View>
-        </View>
-         
-        <TryButton title="Chutar" onPress={() => checkWordSpell()} />
+  const checkWordFill = () => {
+    setShowInput(true);
+    const concatenatedString = inputText.join('');
+    console.log(concatenatedString);
+
+    let textoFormatado = formatarTexto(concatenatedString);
+
+    if (textoFormatado.length === 5) {
+      checkWord(textoFormatado);
+    } else {
+      showMessage({
+        message: "Palavra Incompleta!",
+        description: "Muito burro",
+        type: "warning",
+      });
+    }
+  }
+
+  const checkWord = (word) => {
+    if (word == palavraSecreta) {
+      showMessage({
+        message: "ACERTOU!",
+        description: "",
+        type: "success",
+      });
+
+      novoRowSet = [2, 2, 2, 2, 2];
+      setRowSet(novoRowSet);
+    } else {
+      const novoRowSet = [...rowSet];
+
+      word.split('').forEach((caractere, index) => {
+        atualizarRowSet(caractere, index, novoRowSet);
+      });
+      console.log(novoRowSet);
+      setRowSet(novoRowSet);
+
+      showMessage({
+        message: "ERROU!",
+        description: "Muito burro",
+        type: "info",
+      });
+    }
+  }
+
+  //VIEW
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> {palavraSecreta}</Text>
+
+      <View style={{ flexDirection: 'row', marginTop: 5 }}>
+        {views.map((viewIndex) => (
+          <View
+
+            key={viewIndex}
+            style={[getRectangleStyle(rowSet[viewIndex]), { justifyContent: 'center', alignItems: 'center' }]}
+          >
+            <TextInput
+              style={[styles.input, { width: '70%', textAlign: 'center' }]}
+              placeholder="-"
+              value={inputText[viewIndex] || ''}
+              onChangeText={(value) => handleInputChange(viewIndex, value)}
+              maxLength={1}
+            />
+          </View>
+        ))}
+      </View>
+
+      <TryButton title="Chutar" onPress={() => checkWordFill()} />
 
       <FlashMessage position="top" />
       {/* Códigos sempre dentro da view */}
-      </View>
-    );
-  }
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    rectangle: {
-      width: 70,
-      height: 70,
-      marginHorizontal: 2,
-      backgroundColor: '#EFEFEF',
-      borderWidth: 2,
-      borderColor: '#000000',
-      borderRadius: 10,
-      boxSizing: 'border-box',
-    },
+  rectangle: {
+    width: 70,
+    height: 70,
+    marginHorizontal: 2,
+    backgroundColor: '#EFEFEF',
+    borderWidth: 2,
+    borderColor: '#000000',
+    borderRadius: 10,
+    boxSizing: 'border-box',
+  },
 
-    rectanglePurple: {
-        width: 70,
-        height: 70,
-        marginHorizontal: 2,
-        backgroundColor: '#6959CD', // Cor de fundo laranja
-        borderWidth: 2,
-        borderColor: '#000000',
-        borderRadius: 10,
-        boxSizing: 'border-box',
-      },
+  rectangleGray: {
+    width: 70,
+    height: 70,
+    marginHorizontal: 2,
+    backgroundColor: '#9C9C9C',
+    borderWidth: 2,
+    borderColor: '#000000',
+    borderRadius: 10,
+    boxSizing: 'border-box',
+  },
 
-      rectangleGreen: {
-        width: 70,
-        height: 70,
-        marginHorizontal: 2,
-        backgroundColor: '#2E8B57', // Cor de fundo verde
-        borderWidth: 2,
-        borderColor: '#000000',
-        borderRadius: 10,
-        boxSizing: 'border-box',
-      },
+  rectanglePurple: {
+    width: 70,
+    height: 70,
+    marginHorizontal: 2,
+    backgroundColor: '#6959CD', // Cor de fundo laranja
+    borderWidth: 2,
+    borderColor: '#000000',
+    borderRadius: 10,
+    boxSizing: 'border-box',
+  },
 
-    button: {
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF',
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-        borderWidth: 1,
-        marginTop: 50,
-        borderRadius: 4,
-        paddingVertical: 12, // Adapted from calc(.875rem - 1px)
-        paddingHorizontal: 20, // Adapted from calc(1.5rem - 1px)
-        minHeight: 48, // Converted from 3rem
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.02,
-        shadowRadius: 3,
-        elevation: 2, // Android shadow
-        justifyContent: 'center',
-      },
-      buttonText: {
-        fontFamily: 'system-ui',
-        fontSize: 16,
-        fontWeight: '600',
-        color: 'rgba(0, 0, 0, 0.85)',
-      },
-  });
-  
+  rectangleGreen: {
+    width: 70,
+    height: 70,
+    marginHorizontal: 2,
+    backgroundColor: '#2E8B57', // Cor de fundo verde
+    borderWidth: 2,
+    borderColor: '#000000',
+    borderRadius: 10,
+    boxSizing: 'border-box',
+  },
+
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    borderWidth: 1,
+    marginTop: 50,
+    borderRadius: 4,
+    paddingVertical: 12, // Adapted from calc(.875rem - 1px)
+    paddingHorizontal: 20, // Adapted from calc(1.5rem - 1px)
+    minHeight: 48, // Converted from 3rem
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.02,
+    shadowRadius: 3,
+    elevation: 2, // Android shadow
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontFamily: 'system-ui',
+    fontSize: 16,
+    fontWeight: '600',
+    color: 'rgba(0, 0, 0, 0.85)',
+  },
+});
